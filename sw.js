67f9620d852ce7d75d0d0e8df1402230c0458968
ankitdebnath73-web <ankitdebnath73@gmail.com>
2026-08-10 10:56:30 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'policy-register-v4';
+const CACHE_NAME = 'policy-register-v5';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
